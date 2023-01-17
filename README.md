7ed2a6982da3fea393d5dee7f849ddd785340091
pjungjs <pjungjs@gmail.com>
2023-01-16 22:39:37 -0500

    added price color update feature. updated readme.md file. still work in progress.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Front-end-Portfolio-Project
-My 1st Front-end Portfolio Project from Pursuit Module 2
+# CyberCoin-Project
+My 1st Front-end Portfolio Project from Pursuit 9.4 Class Module 2.
+A website where you can see the live updates of Cryptocurrencies datas.
+
+---
+## How it works
+The code is set up to run every 3 seconds.
+
+On the top of the page, you can see the navigation bar.
+**Home** - Go to the main page.
+**Resources** - Website's data informations, such as API, Bootstrap, etc.
+**About** - Short biography of Me and the link to my GitHub as well as this project repository.
+**Top** - To go to the top of the page.
+**Bottom** - To go to the bottom of the page.
+
+And in the center of the page, you can see the Crypto Live Update table, updating every 3 seconds as stated above.
+It's organized by its _Ranking number_, _Coin name_, _current Price_, _Market Cap_, _Volume in 24hrs_ and _Change Percentage in 24hrs_, **all in USD**.
+It changes the color. To <u>black</u> if the price remains the same, to <u>green</u> if the price went up, and to <u>red</u> if the price went down from previos to current value.
+
+---
+## Features that I'm currently working on
+* submit button, getting the user's input data to search the result accordingly.
+* implement the chart to display on the user search's result.
+* get all the images of the each cryptocurrencies and display it successfully besides of its name. (Currently I have few images, but still working on how to filter through the images on the local folder with the value that I get from the user, and then finally display it.)
+* improve the style of the pages.
+* have pagination on the bottom of the main page, so it doesn't show all top 100 raking crypto, but only about 25 of them, then the rest redirect to the next pages.
+* etc.
+
+
+---
+## Ideas for Future Implementations
+* Implement the user's registrations system.
+* Add "my favorites" page, so the user can quickly view the one's that it's interested.
+* etc.
+
+---
--- js/main.js
+++ js/main.js
@@ -6,94 +6,148 @@ const coinDataBase = {};
 
 const lastPrice = [];
 const currPrice = [];
+const lastMarketCap = [];
+const currMarketCap = [];
+const lastVolume24 = [];
+const currVolume24 = [];
+const lastChangePercent = [];
+const currChangePercent = [];
 
-const formLive = document.querySelector(".live");
-formLive.addEventListener("click", (event) => {
-    event.preventDefault();
-    // coinLiveData();
-    setInterval(coinLiveData, 1000);
-    async function coinLiveData() {
-        await fetch(`${BASE_URL}/assets`)
-        .then(response => response.json())
-        .then(result => {
-            const currentTime = convertUnixTimestamp(result.timestamp);
-            const timeWithoutSec = currentTime.slice(0, 15) + " " + currentTime.slice(19);
+//convert a Unix Timestamp into a human-readable date.
+function convertUnixTimestamp(date) {
+/* A JavaScript date is fundamentally specified as the number of milliseconds 
+that have elapsed since the ECMAScript epoch, 
+which is defined as the midnight at the beginning of January 1, 1970, UTC 
+(equivalent to the UNIX epoch). */
+    return new Date(date).toLocaleString();
+    // const milliseconds = date * 1000; 
+    // const humanDateFormat = new Date(milliseconds);
+    // return humanDateFormat;
+}
 
-            const caption = document.querySelector("table caption");
-            const tableHead = document.querySelector("thead");
-            const tableBody = document.querySelector("tbody");
+// const formLive = document.querySelector(".live");
+// formLive.addEventListener("click", (event) => {
+//     event.preventDefault();
+// });
 
-            caption.textContent = `Live Updates: ${currentTime}`;
-            tableHead.innerHTML = `
-            <tr>
-                <th class="coin rank">Rank</th>
-                <th class="coin name">Coin</th>
-                <th class="coin price">Price</th>
-                <th class="coin marketCap">Market Cap</th>
-                <th class="coin volumeOneDay">Volume 24hr</th>
-                <th class="coin changeOneDay">Change % 24hr</th>
-            </tr>
-            `;
+// coinLiveData();
+setInterval(coinLiveData, 3000);
+let num = 0;
+async function coinLiveData() {
+    await fetch(`${BASE_URL}/assets`)
+    .then(response => response.json())
+    .then(result => {
+        const currentTime = convertUnixTimestamp(result.timestamp);
+        const timeWithoutSec = currentTime.slice(0, 15) + " " + currentTime.slice(19);
 
-            //reset whole table if there's any.
-            resetTable();
+        const caption = document.querySelector("table caption");
+        const tableHead = document.querySelector("thead");
+        const tableBody = document.querySelector("tbody");
+
+        caption.textContent = `Last Updated: ${currentTime} - Updating every 3 seconds.`;
+        tableHead.innerHTML = `
+        <tr>
+            <th class="th coin rank">Rank</th>
+            <th class="th coin name">Coin</th>
+            <th class="th coin price">Price</th>
+            <th class="th coin marketCap">Market Cap</th>
+            <th class="th coin volumeOneDay">Volume 24hr</th>
+            <th class="th coin changeOneDay">Change % 24hr</th>
+        </tr>
+        `;
+
+        //reset whole table if there's any.
+        resetTable();
+
+        for (let i = 0; i < result.data.length; i++) {
+            const { id, rank, name, priceUsd, symbol, explorer, vwap24Hr,
+                marketCapUsd, volumeUsd24Hr, changePercent24Hr } = result.data[i];
+            const arrOfPrices = [
+                Number(priceUsd).toFixed(6),
+                Number(marketCapUsd).toLocaleString("en-US"),
+                Number(volumeUsd24Hr).toLocaleString("en-US"),
+                Number(changePercent24Hr).toLocaleString("en-US")
+            ];
+
+            //adding data to the Coin Data Base
+            coinDataBase[symbol] = id;
             
-            for (let i = 0; i < result.data.length; i++) {
-                const { id, rank, name, priceUsd, symbol, explorer, vwap24Hr,
-                    marketCapUsd, volumeUsd24Hr, changePercent24Hr } = result.data[i];
-                const arrOfPrices = [
-                    Number(priceUsd).toLocaleString("en-US"),
-                    Number(marketCapUsd).toLocaleString("en-US"),
-                    Number(volumeUsd24Hr).toLocaleString("en-US"),
-                    Number(changePercent24Hr).toLocaleString("en-US")
-                ];
-                
-                //adding data to the Coin Data Base
-                coinDataBase[symbol] = id;
-
-                // <td class="coin name">
-                //     <img class="coin img-fluid" alt="logo" src="../assets/crypto-img/bitcoin-btc-logo.png" width="30">
-                //     <a href="${explorer}" target="_blank" rel="noopener noreferrer">${name}</a> (${symbol})
-                // </td>
-                /** <a href="#" target="_blank" rel="noopener noreferrer">${name}</a>
-                * Setting it to "noopener noreferrer" is to prevent a type of phishing known as tabnabbing. **/
-
-                const tr = document.createElement("tr")
-                tr.classList.toggle(`row${i}`);
-                tr.innerHTML = `
-                <td class="coin rank">${rank}</td>
-                <td class="coin name">
-                    <img class="coin img-fluid" alt="logo" src="../assets/crypto-img/bitcoin-btc-logo.png" width="30">
-                    <a href="${explorer}" target="_blank" rel="noopener noreferrer">${name}</a> (${symbol})
-                </td>
-                <td class="coin price" style="color:${priceChangeColor(lastPrice[i], priceUsd)};">$ ${arrOfPrices[0]}</td>
-                <td class="coin marketCap" ${priceChangeColor(lastPrice[i], marketCapUsd)};">$ ${arrOfPrices[1]}</td>
-                <td class="coin volumeOneDay" ${priceChangeColor(lastPrice[i], volumeUsd24Hr)}};">$ ${arrOfPrices[2]}</td>
-                <td class="coin changeOneDay" ${priceChangeColor(lastPrice[i], changePercent24Hr)};">${arrOfPrices[3]}%</td>
-                `;
-                tableBody.appendChild(tr);
-                
-                tr.addEventListener("click", event => {
-                    event.preventDefault;
-                    // console.log(event.target) // logs clicked td element
-                    // console.log(event.target.getAttribute("class")) // logs clicked element's class
-                    // console.log(event.path[1].getAttribute("class")) // logs clicked table row's class
-                });
-
-                // if(lastPrice.length <= 100) {
-                //     lastPrice[i] = priceUsd;
-                // } else if(currPrice.length === 0) {
-                //     currPrice[i] = priceUsd;
-                // }
+            if(num === 0) {
+                lastPrice[i] = priceUsd;
+                lastMarketCap[i] = marketCapUsd;
+                lastVolume24[i] = volumeUsd24Hr;
+                lastChangePercent[i] = changePercent24Hr;
+            } else if(num === 1) {
+                currPrice[i] = priceUsd;
+                currMarketCap[i] = marketCapUsd;
+                currVolume24[i] = volumeUsd24Hr;
+                currChangePercent[i] = changePercent24Hr;
+            } else if(num === 2) {
+                lastPrice[i] = currPrice[i];
+                lastMarketCap[i] = currMarketCap[i];
+                lastVolume24[i] = currVolume24[i];
+                lastChangePercent[i] = currChangePercent[i];
+                currPrice[i] = priceUsd;
+                currMarketCap[i] = marketCapUsd;
+                currVolume24[i] = volumeUsd24Hr;
+                currChangePercent[i] = changePercent24Hr;
             }
-            // if(lastPrice.length > 0 && currPrice.length > 0) {
-            //     priceChangeColor(...lastPrice, ...currPrice)
-            // }
-            // const coinPrice = document.querySelectorAll("td.coin.price");
-        })
-        .catch((error) => alert(error));
+
+            // <td class="coin name">
+            //     <img class="coin img-fluid" alt="logo" src="../assets/crypto-img/bitcoin-btc-logo.png" width="30">
+            // </td>
+            /** <a href="#" target="_blank" rel="noopener noreferrer">${name}</a>
+            * Setting it to "noopener noreferrer" is to prevent a type of phishing known as tabnabbing. **/
+
+            const tr = document.createElement("tr")
+            tr.classList.toggle(`row${i}`);
+            tr.innerHTML = `
+            <td class="coin rank">${rank}</td>
+            <td class="coin name">
+                <img class="coin img-fluid" alt="logo" src="../assets/crypto-img/bitcoin-btc-logo.png" width="30">
+                <a href="${explorer}" target="_blank" rel="noopener noreferrer">${name}</a> (${symbol})
+            </td>
+            <td class="tb coin price" style="color:${priceChangeColor(lastPrice[i], currPrice[i])};">$ ${arrOfPrices[0]}</td>
+            <td class="tb coin marketCap" style="color:${priceChangeColor(lastMarketCap[i], currMarketCap[i])};">$ ${arrOfPrices[1]}</td>
+            <td class="tb coin volumeOneDay" style="color:${priceChangeColor(lastVolume24[i], currVolume24[i])}};">$ ${arrOfPrices[2]}</td>
+            <td class="tb coin changeOneDay" style="color:${priceChangeColor(lastChangePercent[i], currChangePercent[i])};">${arrOfPrices[3]}%</td>
+            `;
+            tableBody.appendChild(tr);
+
+            tr.addEventListener("click", event => {
+                event.preventDefault;
+                // console.log(event.target) // logs clicked td element
+                // console.log(event.target.getAttribute("class")) // logs clicked element's class
+                // console.log(event.path[1].getAttribute("class")) // logs clicked table row's class
+            });
+        }
+
+        if(num === 0 || num === 2) {
+            num = 1;
+        } else if(num === 1) {
+            num = 2;
+        }
+    })
+    .catch((error) => console.log(error));
+}
+
+
+function resetTable() {
+    const tableBody = document.querySelector("tbody");
+    if(tableBody.innerText.length > 0) {        
+        let child = tableBody.firstChild; 
+        while (child) {
+            tableBody.removeChild(child);
+            child = tableBody.firstChild;
+        }
     }
-});
+}
+
+
+function priceChangeColor(last, current) {
+    return !current || last === current ? "black" : current > last ? "green" : "red";
+}
+
 
 const formSearch = document.querySelector(".submitAll");
 formSearch.addEventListener("submit", (event) => {
@@ -117,29 +171,6 @@ formSearch.addEventListener("submit", (event) => {
     history(userCoinId, userTime);
 });
 
-//convert a Unix Timestamp into a human-readable date.
-function convertUnixTimestamp(date) {
-/* A JavaScript date is fundamentally specified as the number of milliseconds 
-that have elapsed since the ECMAScript epoch, 
-which is defined as the midnight at the beginning of January 1, 1970, UTC 
-(equivalent to the UNIX epoch). */
-    return new Date(date).toLocaleString();
-    // const milliseconds = date * 1000; 
-    // const humanDateFormat = new Date(milliseconds);
-    // return humanDateFormat;
-}
-
-function resetTable() {
-    const tableBody = document.querySelector("tbody");
-    if(tableBody.innerText.length > 0) {        
-        let child = tableBody.firstChild; 
-        while (child) {
-            tableBody.removeChild(child);
-            child = tableBody.firstChild;
-        }
-    }
-}
-
 
 async function history(userCoinId, userTime) {
     await fetch(`https://api.coincap.io/v2/assets/${userCoinId}/history?interval=${userTime}`)
@@ -155,36 +186,11 @@ async function history(userCoinId, userTime) {
             coinSearchPrice.push(priceUsd);
             coinSearchTime.push(currentTime);
         }
-        // createChart(coinSearchPrice, coinSearchTime);
-        console.log(coinSearchPrice, coinSearchTime)
+        // console.log(coinSearchPrice, coinSearchTime)
+        createChart(coinSearchPrice, coinSearchTime);
     })
     .catch((error) => alert(error));
 }
 
 
-function priceChangeColor(lastPrice, currPrice) {
-    !lastPrice || lastPrice === currPrice ? "black" : currPrice > lastPrice ? "green" : "red";
-}
-
-
-// function createChart(coinSearchPrice, coinSearchTime) {
-//     console.log(coinSearchPrice, coinSearchTime);
-//     const ctx = document.querySelector("#myChart").getContext("2d");
-//     //Setup block
-//     const labels = [...coinSearchTime];
-//     const datas = {
-//         labels,
-//         datasets: [{
-//           label: "Price Change",
-//           borderWidth: 1,
-//           data: [...coinSearchPrice]
-//         }]
-//     };
-//     //Config block
-//     const config = {
-//         type: "line",
-//         datas,
-//         options: {}
-//     };
-//     const newChart = new Chart(ctx, config);
-// }
+function createChart(coinSearchPrice, coinSearchTime) {}
